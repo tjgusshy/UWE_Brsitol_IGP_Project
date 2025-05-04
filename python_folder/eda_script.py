6c13ca4f5265894f1455e4175e3f641873c53d78
e <tjt163787@gmail.com>
2025-05-04 08:50:12 +0100

    t

--- python_folder/eda_script.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import numpy as np
-import seaborn as sn
-import matplotlib.pyplot as matplot
-
-xls = pd.ExcelFile('csv_data/data1.xlsx')
-demo_df = pd.read_excel(xls,'Demographics')
-demo_df
-
