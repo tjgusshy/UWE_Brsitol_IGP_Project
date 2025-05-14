7c2de8bb16ce0ca394bc01b790f84bf00f3f35e2
e <tjt163787@gmail.com>
2025-05-14 16:28:20 +0100

    updated file

--- python_folder/eda_script.py
+++ python_folder/eda_script.py
@@ -2,8 +2,19 @@ import pandas as pd
 import numpy as np
 import seaborn as sn
 import matplotlib.pyplot as matplot
+from sqlalchemy import create_engine, text
+import os
+import sqlalchemy.exc
+import logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
 
-xls = pd.ExcelFile('csv_data/data1.xlsx')
-demo_df = pd.read_excel(xls,'Demographics')
-demo_df
 
+
+
+
+#Create a connection string
+connection_string = f'mysql+{db_name}://{username}:{password}@{host}/{db_name}'
+
+db_connection = create_engine(connection_string)
