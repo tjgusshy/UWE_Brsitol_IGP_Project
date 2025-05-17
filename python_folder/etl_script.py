08fba0b1c432ca15bcbed734b825080760b4382f
e <tjt163787@gmail.com>
2025-05-17 13:19:46 +0100

    new update

--- python_folder/etl_script.py
+++ python_folder/etl_script.py
@@ -26,33 +26,32 @@ except Exception as e:
     logging.error(f"Error connecting to the database: {e}")
     raise
 
-# # # read in the merged data set from csv file
-# students_info = pd.read_csv('python_folder/students_info.csv')
-# print(students_info.shape)
+# # read in the merged data set from csv file
+students_info = pd.read_csv('python_folder/students_info.csv')
+print(students_info.shape)
+# # # # writing into mysql database
+students_info.to_sql('students_info', con=db_connection, if_exists='replace', index=False)
+
+
 # # # # # writing into mysql database
-# students_info.to_sql('students_info', con=db_connection, if_exists='replace', index=False)
+merged_weeks = pd.read_csv('python_folder/merged_weeks.csv')
+print(merged_weeks.shape)
+
+try:
+    merged_weeks.to_sql('merged_weeks', con=db_connection, if_exists='replace', index=False)
+except Exception as e:
+    logging.error(f"Error writing to the database: {e}")
+    raise
 
 
-# # # # # # writing into mysql database
-# merged_weeks = pd.read_csv('python_folder/merged_weeks.csv')
-# print(merged_weeks.shape)
-
-# try:
-#     merged_weeks.to_sql('merged_weeks', con=db_connection, if_exists='replace', index=False)
-# except Exception as e:
-#     logging.error(f"Error writing to the database: {e}")
-#     raise
-
-
-demo_week_2 = pd.read_csv('python_folder/demo_week_2.csv')
-print(demo_week_2.shape)
+demo_week = pd.read_csv('python_folder/demo_week_2.csv')
 # # # writing into mysql database
 
 
 # # # read in the resources data set from csv file
 
 try:
-   demo_week_2.to_sql('demo_2', con=db_connection, if_exists='replace', index=False)
+   demo_week.to_sql('demography', con=db_connection, if_exists='replace', index=False)
 except Exception as e:
     logging.error(f"Error writing to the database: {e}")
     raise
@@ -62,19 +61,21 @@ demo_numeric = pd.read_csv('python_folder/demo_numeric.csv')
 print(demo_numeric.shape)
 # # # writing into mysql database
 
-
 # # # read in the resources data set from csv file
-
 try:
    demo_numeric.to_sql('demo_numeric', con=db_connection, if_exists='replace', index=False)
 except Exception as e:
-    logging.error(f"Error writing to the database: {e}")
+    logging.error(f"Error writing to the database:{e}")
     raise
 
+Retrospective = pd.read_csv('python_folder/Retrospective.csv')
 
-# retro_numeric = pd.read_csv('python_folder/csv_data/Retro_numeric.csv')
+# writing into mysql database
+Retrospective.to_sql('retrospective', con=db_connection, if_exists='replace', index=False)  
+# read in the 
 
-# # # writing into mysql database
+retro_numeric = pd.read_csv('python_folder/retro_numeric.csv')
 
-# retro_numeric.to_sql('retro_numeric', con=db_connection, if_exists='replace', index=False)  
-# # read in the 
+# writing into mysql database
+retro_numeric.to_sql('retro_numeric', con=db_connection, if_exists='replace', index=False)
+# read in the
